c8405f0eaa16b383a41acc795788a7d85a9a5376
Chris Nady <nady.christopher@gmail.com>
2017-09-06 15:22:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,10 @@ katz_deli = []
 
 def line(deli)
   if deli==[]
-    puts "There is nobody in line"
+    puts "The line is currently empty."
   else
-    puts deli
+    puts "The line is currently: 1. #{deli[1]} 2. #{deli[2]} 3. #{deli[3]}"
   end
 end
+
+
